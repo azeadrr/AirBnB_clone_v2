2367fdc869b28a7a93a2aaf396ee3e9317bda93f
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-10-05 19:34:27 -0700

    mandatory tasks

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
-"""Fabric script that distributes
-an archive to your web servers"""
-
+"""Fabric script that distributes"""
 from fabric.contrib import files
 from fabric.api import env, put, run
 import os
 
-env.hosts = ['54.144.128.184', '100.25.159.254']
+env.hosts = ['54.234.81.198', '100.25.193.190']
 
 
 def do_deploy(archive_path):
-    """deployment function"""
+    """Deploy archive!"""
     if not os.path.exists(archive_path):
         return False
 
-    data_path = '/data/web_static/releases/'
+    dt_path = '/data/web_static/releases/'
     tmp = archive_path.split('.')[0]
-    name = tmp.split('/')[1]
-    dest = data_path + name
+    nm = tmp.split('/')[1]
+    dst = dt_path + nm
 
     try:
         put(archive_path, '/tmp')
-        run('mkdir -p {}'.format(dest))
-        run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
-        run('rm -f /tmp/{}.tgz'.format(name))
-        run('mv {}/web_static/* {}/'.format(dest, dest))
-        run('rm -rf {}/web_static'.format(dest))
+        run('mkdir -p {}'.format(dst))
+        run('tar -xzf /tmp/{}.tgz -C {}'.format(nm, dst))
+        run('rm -f /tmp/{}.tgz'.format(nm))
+        run('mv {}/web_static/* {}/'.format(dst, dst))
+        run('rm -rf {}/web_static'.format(dst))
         run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(dest))
+        run('ln -s {} /data/web_static/current'.format(dst))
         return True
     except Exception:
         return False
