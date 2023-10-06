1b949923c75345f52819e8419e95aa079d0a5bc0
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-10-05 18:07:30 -0700

    mandatory tasks

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """Fabric script that generates a .tgz
 archive from contents of the web_static"""
-
-from datetime import datetime
 from fabric.api import local
-from os import path
+from datetime import datetime
 
 def do_pack():
     """Compress before sending"""
