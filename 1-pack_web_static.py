d4485b892b4741f2c32a8a585d5769b8078050b9
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-10-05 18:12:06 -0700

    mandatory tasks

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,9 +8,9 @@ def do_pack():
     """function to generate the archive"""
     try:
         local("mkdir -p versions")
-        reqs = local("tar -cvzf versions/web_static_{}.tgz web_static"
+        res = local("tar -cvzf versions/web_static_{}.tgz web_static"
                     .format(datetime.now().strftime("%Y%m%d%H%M%s")),
                     capture=True)
-        return reqs
+        return res
     except Exception:
         return None
