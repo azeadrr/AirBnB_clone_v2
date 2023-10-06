ed4c487276eca78563f13dcace8b91211aa47ad7
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-10-05 18:43:55 -0700

    mandatory tasks

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 """Fabric script that generates a .tgz archive"""
-from fabric.api import local
 from datetime import datetime
-
+from fabric.api import local
 
 def do_pack():
     """Compress before sending"""
     try:
         local("mkdir -p versions")
         res = local("tar -cvzf versions/web_static_{}.tgz web_static"
-                    .format(datetime.now().strftime("%Y%m%d%H%M%s")),
-                    capture=True)
+                    .format(datetime.now().strftime("%Y%m%d%H%M%s")),capture=True)
         return res
     except Exception:
         return None
